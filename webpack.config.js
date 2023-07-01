ec955148921e1a0a3cbb9a44531ff204f3535081
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-07-01 21:51:54 +0300

    feat: add project environment

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const isProduction = process.env.NODE_ENV == 'production';
+
+const config = {
+  target: 'node',
+  entry: './src/index.ts',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: 'ts-loader',
+        exclude: ['/node_modules/'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+  },
+  experiments: {
+    topLevelAwait: true,
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  } else {
+    config.mode = 'development';
+  }
+  return config;
+};

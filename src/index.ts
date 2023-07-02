b6b15747903635a8eacd0d2bc3393bd8ee64e37f
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-07-02 22:19:00 +0300

    feat: move port to .env

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 4000;
+const port = process.env.PORT;
 
 const server = http.createServer((_req: Request, res: Response) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
 export type User = {
+  id: string;
   username: string;
   age: number;
   hobbies: string[];

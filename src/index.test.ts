0d681987f9909f16ce02660190e1206b9cabdb25
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-07-03 23:50:36 +0300

    feat: add second test scenario

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,6 @@
 import supertest from 'supertest';
 import { server } from './index';
+import { ERROR_MESSAGES } from './constants';
 
 const mockUser = {
   username: 'John',
@@ -12,6 +13,15 @@ const updatedUser = {
   age: 35,
   hobbies: ['gardening'],
 };
+
+const fakeUser = {
+  username: 21,
+  age: 'fresh and young',
+  hobbies: 'dancing',
+};
+
+const fakeId = 12345;
+const fakeUuid = '11111111-1111-1111-1111-111111111111';
 
 const request = supertest.agent(server);
 
@@ -56,5 +66,36 @@ describe('crud-spi', () => {
     const deletedUserReq = await request.get(`/api/users/${id}`);
 
     expect(deletedUserReq.status).toBe(404);
+  });
+
+  test('should perform error messages with incorrect requests', async () => {
+    //incorrect url
+    const res = await request.get('/api/books');
+
+    expect(res.status).toBe(404);
+    expect(res.body).toEqual({ message: ERROR_MESSAGES.pageNotFound });
+
+    //incorrect id (not uuid)
+
+    const fakeIdReq = await request.get(`/api/users/${fakeId}`);
+
+    expect(fakeIdReq.status).toBe(400);
+    expect(fakeIdReq.body).toEqual({ message: ERROR_MESSAGES.invalidId });
+
+    //nonexistent user
+    const createReq = await request.post('/api/users').send(JSON.stringify(mockUser));
+
+    expect(createReq.status).toBe(201);
+
+    const fakeUuidReq = await request.get(`/api/users/${fakeUuid}`);
+
+    expect(fakeUuidReq.status).toBe(404);
+    expect(fakeUuidReq.body).toEqual({ message: ERROR_MESSAGES.userNotFound });
+
+    //creating user with invalid request body
+    const createFakeUserReq = await request.post('/api/users').send(JSON.stringify(fakeUser));
+
+    expect(createFakeUserReq.status).toBe(400);
+    expect(createFakeUserReq.body).toEqual({ message: ERROR_MESSAGES.invalidFields });
   });
 });
